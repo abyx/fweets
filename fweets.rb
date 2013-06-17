c1d9ddd4fc4cac1b1de3341ecea2f50e1d19a717
Aviv Ben-Yosef <aviv.by@gmail.com>
2013-06-17 20:10:07 +0300

    make tweets feed get user from url

--- fweets.rb
+++ fweets.rb
@@ -2,22 +2,22 @@ require 'sinatra/base'
 require 'twitter'
 require 'rss'
 
-USER = 'avivby'
-
 class Fweets < Sinatra::Base
-  get '/' do
+  get '/tweets/:user_id' do
+    user = params[:user_id]
+
     content_type "application/rss+xml; charset=utf-8"
-    tweets = Twitter.user_timeline(USER)
+    tweets = Twitter.user_timeline(user)
     rss = RSS::Maker.make("2.0") do |maker|
-      maker.channel.author = USER
+      maker.channel.author = user
       maker.channel.updated = Time.now.to_s
-      maker.channel.title = USER
-      maker.channel.description = "@#{USER}"
-      maker.channel.link = "http://twitter.com/#{USER}"
+      maker.channel.title = "@#{user} tweets"
+      maker.channel.description = "@#{user} tweets"
+      maker.channel.link = "http://twitter.com/#{user}"
 
       tweets.each do |tweet|
         maker.items.new_item do |item|
-          item.link = "http://twitter.com/#{USER}/status/#{tweet.id}"
+          item.link = "http://twitter.com/#{tweet.from_user}/status/#{tweet.id}"
           item.title = tweet.full_text
           item.description = tweet.full_text
           item.updated = tweet.created_at
