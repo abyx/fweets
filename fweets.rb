6f7349be33518ec74c705605503c7b2005f702c0
Aviv Ben-Yosef <aviv.by@gmail.com>
2013-06-17 20:18:40 +0300

    clean up

--- fweets.rb
+++ fweets.rb
@@ -3,17 +3,15 @@ require 'twitter'
 require 'rss'
 
 class Fweets < Sinatra::Base
-  get '/tweets/:user' do
-    user = params[:user]
+  def to_rss(tweets, user, title, link)
 
     content_type "application/rss+xml; charset=utf-8"
-    tweets = Twitter.user_timeline(user)
     rss = RSS::Maker.make("2.0") do |maker|
       maker.channel.author = user
       maker.channel.updated = Time.now.to_s
-      maker.channel.title = "@#{user} tweets"
-      maker.channel.description = "@#{user} tweets"
-      maker.channel.link = "http://twitter.com/#{user}"
+      maker.channel.title = title
+      maker.channel.description = title
+      maker.channel.link = link
 
       tweets.each do |tweet|
         maker.items.new_item do |item|
@@ -31,31 +29,21 @@ class Fweets < Sinatra::Base
     rss.to_s
   end
 
+  get '/tweets/:user' do
+    user = params[:user]
+
+    to_rss(Twitter.user_timeline(user),
+           user,
+           "@#{user} tweets",
+           "http://twitter.com/#{user}")
+  end
+
   get '/favs/:user' do
     user = params[:user]
 
-    content_type "application/rss+xml; charset=utf-8"
-    tweets = Twitter.favorites(user)
-    rss = RSS::Maker.make("2.0") do |maker|
-      maker.channel.author = user
-      maker.channel.updated = Time.now.to_s
-      maker.channel.title = "@#{user} favorites"
-      maker.channel.description = "@#{user} favorites"
-      maker.channel.link = "http://twitter.com/#{user}/favorites"
-
-      tweets.each do |tweet|
-        maker.items.new_item do |item|
-          item.link = "http://twitter.com/#{tweet.from_user}/status/#{tweet.id}"
-          text = "@#{tweet.from_user}: #{tweet.full_text}"
-          item.title = text
-          item.description = text
-          item.updated = tweet.created_at
-          item.guid.content = tweet.id.to_s
-          item.author = tweet.from_user
-        end
-      end
-    end
-
-    rss.to_s
+    to_rss(Twitter.favorites(user),
+           user,
+           "@#{user} favorites",
+           "http://twitter.com/#{user}/favorites")
   end
 end
